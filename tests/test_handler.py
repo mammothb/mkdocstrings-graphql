28ff64a4d5160f9700b38ed9236a2bfe63ba1958
mmb L <7960706+mammothb@users.noreply.github.com>
2025-03-31 17:53:37 +0800

    build: migrate test runner

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -7,8 +7,3 @@ from mkdocstrings_handlers.graphql import GraphQLHandler, GraphQLOptions
 def test_collect_empty_identifier(handler: GraphQLHandler) -> None:
     with pytest.raises(CollectionError, match="Empty identifier"):
         handler.collect("", GraphQLOptions())
-
-
-# def test_collect_missing_module(handler: GraphQLHandler) -> None:
-#     with pytest.raises(CollectionError):
-#         handler.collect("aaa", GraphQLOptions())
