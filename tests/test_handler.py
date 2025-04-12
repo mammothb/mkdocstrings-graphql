60bfcdf99a1adc2e1ab85b1b4f4f385c1dda4f66
mmb L <7960706+mammothb@users.noreply.github.com>
2025-04-12 13:43:06 +0800

    test: test invalid kind local option

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -8,3 +8,17 @@ from mkdocstrings_handlers.graphql import GraphQLHandler, GraphQLOptions
 def test_collect_invalid_identifier(identifier: str, handler: GraphQLHandler) -> None:
     with pytest.raises(CollectionError, match=f"Failed to parse identifier '{identifier}'"):
         handler.collect(identifier, GraphQLOptions())
+
+
+@pytest.mark.parametrize("identifier", ["schema.*", "schema.prefix.*"])
+def test_collect_invalid_wildcard_directive(identifier: str, handler: GraphQLHandler) -> None:
+    handler._schemas_collection["schema"] = {}
+    with pytest.raises(CollectionError, match="Local configuration 'kind' must be set when using wildcard directive"):
+        handler.collect(identifier, GraphQLOptions())
+
+
+@pytest.mark.parametrize("identifier", ["schema.*", "schema.prefix.*"])
+def test_collect_invalid_kind_value(identifier: str, handler: GraphQLHandler) -> None:
+    handler._schemas_collection["schema"] = {}
+    with pytest.raises(CollectionError, match="'kind' must be one of"):
+        handler.collect(identifier, GraphQLOptions(kind="INVALID"))
